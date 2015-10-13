00046b73383a0dc27a7a13395bdfa8a252670039
Eric Henderson <henderea@gmail.com>
2015-10-13 10:07:57 -0400

    improvements to Ask.ask_yn

--- lib/everyday-cli-utils/ask.rb
+++ lib/everyday-cli-utils/ask.rb
@@ -2,33 +2,32 @@ require 'readline'
 
 module EverydayCliUtils
   module Ask
-    def self::setup_options(options)
+    def self.setup_options(options)
       mapped = []
       options.each_with_index { |v, k|
-        mapped << "#{k+1}) #{v.to_s}"
+        mapped << "#{k + 1}) #{v}"
       }
       mapped.join("\n")
     end
 
-    def self::ask(question, options, &block)
+    def self.ask(question, options, &block)
       val = '-1'
-      while true
+      loop do
         print "#{question}\n\n#{setup_options(options)}\n\n"
         val = Readline.readline("Please enter your choice (1 - #{options.count}): ", true)
-        if !(val =~ /^\d+$/).nil? && (1..options.count).include?(val.to_i)
-          break
-        end
+        break if !(val =~ /^\d+$/).nil? && (1..options.count).include?(val.to_i)
         print "\n\nYou must enter an integer between 1 and #{options.count}. Please try again.\n\n"
       end
       block.call(options[val.to_i - 1])
     end
 
-    def self::ask_yn(question, &block)
-      resp = Readline.readline("#{question} (yes/no) ", true)
-      block.call(resp.downcase == 'yes' || resp.downcase == 'y')
+    def self.ask_yn(question, options = {}, &block)
+      resp = Readline.readline("#{question} ([y]es/[n]o) ", true)
+      val = resp.downcase == 'yes' || resp.downcase == 'y'
+      block.call(val) if !options[:only] || options[:only] == (val ? :yes : :no)
     end
 
-    def self::hash_to_options(hash, extra = [])
+    def self.hash_to_options(hash, extra = [])
       hash.keys + extra
     end
   end
